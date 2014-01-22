8ec485fc3f53e0614933f1dc7648be6444d13cb8
Ryan MacGillivray <rmacg89@gmail.com>
2014-01-22 22:22:34 +0000

    disallow double dots in emails and tidy up before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   validates_presence_of :name
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
   validates :password, length: { minimum: 6 }
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   has_secure_password
 end
