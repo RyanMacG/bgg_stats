a45b11a6a12580e41e557f03f5c7672093e6b383
Ryan MacGillivray <rmacg89@gmail.com>
2014-04-08 21:51:42 +0100

    fix typo in flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to  BGG Statsa"
+      flash[:success] = "Welcome to BGG Stats"
       redirect_to @user
     else
       render 'new'
