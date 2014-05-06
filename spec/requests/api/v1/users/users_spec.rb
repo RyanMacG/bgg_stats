ce6ae2f04463be696e49db0c1a30980c8b8696a2
Ryan MacGillivray <rmacg89@gmail.com>
2014-05-06 21:28:42 +0100

    test for user creation via api and failure

--- spec/requests/api/v1/users/users_spec.rb
+++ spec/requests/api/v1/users/users_spec.rb
@@ -15,4 +15,36 @@ describe "User API requests" do
       )
     end
   end
+
+  describe "POST /v1/users" do
+    it "should create a user" do
+      post '/v1/users', {
+        name:                   'Foo bar',
+        email:                  'bar@baz.com',
+        password:               'foobar',
+        password_confirmation:  'foobar'
+      }.to_json, { 'Content-Type' => 'application/json' }
+
+      user = User.last
+      expect(response_json).to eq({ 'name' => user.name, 'email' => user.email })
+    end
+
+    it "returns an error when invalid" do
+      post '/v1/users',
+      {}.to_json,
+      { 'Content-Type' => 'application/json' }
+
+      expect(response_json).to eq({
+        'message' => 'Validation Failed',
+        'errors' => [
+          "Name can't be blank",
+          "Email can't be blank",
+          "Email is invalid",
+          "Password is too short (minimum is 6 characters)",
+          "Password can't be blank",
+        ]
+      })
+      expect(response.code.to_i).to eq 422
+    end
+  end
 end
