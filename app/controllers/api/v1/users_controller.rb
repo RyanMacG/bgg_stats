6e02f6615e193e69abd31cc47d5cdc267f0eee7d
Ryan MacGillivray <rmacg89@gmail.com>
2014-05-06 21:31:43 +0100

    updated users api controller to handle user creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,29 @@
-class Api::V1::UsersController < ApplicationController
+class Api::V1::UsersController < ApiController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      render
+    else
+      render json: {
+        message: 'Validation Failed',
+        errors: @user.errors.full_messages
+      }, status: 422
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     @page_title = @user.name
   end
+
+  private
+    def user_params
+      {
+        name: params[:name],
+        email: params[:email],
+        password: params[:password],
+        password_confirmation: params[:password_confirmation]
+      }
+    end
 end
